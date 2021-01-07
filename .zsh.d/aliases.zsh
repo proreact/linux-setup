68f962e40b71429ad58c1bac72b6bb3916071f5a
Kris Lux <krl@proreact.dk>
2021-01-07 11:10:20 +0100

    Add git latest commits alias

--- .zsh.d/aliases.zsh
+++ .zsh.d/aliases.zsh
@@ -1,4 +1,7 @@
 alias reloadzsh="source ~/.zshrc"
 alias vi="vim"
+
 alias glog='git log --pretty=format:"%C(yellow)%h%Creset %C(green)%an%Creset %C(blue)(%ar)%Creset%n%s"'
+alias glc="git for-each-ref --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
+
 alias apts='apt search --names-only'
